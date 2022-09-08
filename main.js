226cde34a9948098b5212a507d3a988a3616590c
andrews-zack <zachary.andrews.au19@gmail.com>
2022-09-08 12:21:47 +0000

    adding variables and name part of function

--- main.js
+++ main.js
@@ -9,20 +9,29 @@ function randomValueFromArray(array){
 
 let storyText = 'It was 94 fahrenheit outside, so :insertx: went for a walk. When they got to :inserty:, they stared in horror for a few moments, then :insertz:. Bob saw the whole thing, but was not surprised — :insertx: weighs 300 pounds, and it was a hot day.';
 let insertX = [
-    'Willy the Goblin'
-    'Big Daddy'
+    'Willy the Goblin',
+    'Big Daddy',
     'Father Christmas'
 ];
 let insertY = [
-    'the soup kitchen'
-    'Disneyland'
+    'the soup kitchen',
+    'Disneyland',
     'the White House'
 ];
 let insertZ = [
-    'spontaneously combusted'
-    'melted into a puddle on the sidewalk'
+    'spontaneously combusted',
+    'melted into a puddle on the sidewalk',
     'turned into a slug and crawled away'
 ];
+
+let newStory = storyText;
+let xItem = randomValueFromArray(insertX);
+let yItem = randomValueFromArray(insertY);
+let zItem = randomValueFromArray(insertZ);
+
+newStory = newStory.replaceAll(':insertX:', xItem);
+newStory = newStory.replace(':insertY:', yItem);
+newStory = newStory.replace(':insertZ:', zItem);
 
 randomize.addEventListener('click', result);
 
@@ -30,6 +39,7 @@ function result() {
 
   if(customName.value !== '') {
     const name = customName.value;
+    newStory.replaceAll('Bob', name);
 
   }
 
