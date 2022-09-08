55e886c6aab588a50a41a06afef6bedd02932886
andrews-zack <zachary.andrews.au19@gmail.com>
2022-09-08 14:50:36 +0000

    retrying id for randomize, changing querySelector to getElementById

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
 const customName = document.getElementById('customname');
-const randomize = document.querySelector('.randomize');
+const randomize = document.getElementById('randomize');
 const story = document.querySelector('.story');
 
 function randomValueFromArray(array){
