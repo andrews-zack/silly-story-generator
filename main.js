6b95d6a4ec2506663b83e4bfc9ca9edd74491c97
andrews-zack <zachary.andrews.au19@gmail.com>
2022-09-08 12:39:49 +0000

    fixed syntax error in temperature conversion

--- main.js
+++ main.js
@@ -45,7 +45,7 @@ function result() {
 
   if(document.getElementById("uk").checked) {
     const weight = Math.round(300/14) + ' stone';
-    const temperature =  Math.round((94-32)8(5/9)) + ' celsius';
+    const temperature =  Math.round((94-32)*(5/9)) + ' celsius';
     newStory.replace('94 fahrenheit', temperature);
     newStory.replace('300 pounds', weight);
   }
