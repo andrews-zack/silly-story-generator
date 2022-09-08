2847a7f3a0aa6876ee06082a29bea31d2cc6b3a8
andrews-zack <zachary.andrews.au19@gmail.com>
2022-09-08 15:52:40 +0000

    adding replace methods inside function so it is repeatable

--- main.js
+++ main.js
@@ -7,28 +7,36 @@ function randomValueFromArray(array){
   return array[random];
 }
 
-let storyText = 'It was 94 fahrenheit outside, so :insertx: went for a walk. When they got to :inserty:, they stared in horror for a few moments, then :insertz:. Bob saw the whole thing, but was not surprised — :insertx: weighs 300 pounds, and it was a hot day.';
-let insertX = [
+const storyText = 'It was 94 fahrenheit outside, so :insertx: went for a walk. When they got to :inserty:, they stared in horror for a few moments, then :insertz:. Bob saw the whole thing, but was not surprised — :insertx: weighs 300 pounds, and it was a hot day.';
+const insertX = [
     'Willy the Goblin',
     'Big Daddy',
     'Father Christmas'
 ];
-let insertY = [
+const insertY = [
     'the soup kitchen',
     'Disneyland',
     'the White House'
 ];
-let insertZ = [
+const insertZ = [
     'spontaneously combusted',
     'melted into a puddle on the sidewalk',
     'turned into a slug and crawled away'
 ];
 
-let newStory = storyText;
-
 randomize.addEventListener('click', result);
 
 function result() {
+
+  let newStory = storyText;
+
+  const xItem = randomValueFromArray(insertX);
+  const yItem = randomValueFromArray(insertY);
+  const zItem = randomValueFromArray(insertZ);
+  
+  newStory = newStory.replaceAll(':insertx:', xItem);
+  newStory = newStory.replace(':inserty:', yItem);
+  newStory = newStory.replace(':insertz:', zItem);
 
   if(customName.value !== '') {
     const name = customName.value;
@@ -41,14 +49,6 @@ function result() {
     newStory = newStory.replace('94 fahrenheit', temperature);
     newStory = newStory.replace('300 pounds', weight);
   }
-
-  const xItem = randomValueFromArray(insertX);
-  const yItem = randomValueFromArray(insertY);
-  const zItem = randomValueFromArray(insertZ);
-  
-  newStory = newStory.replaceAll(':insertx:', xItem);
-  newStory = newStory.replace(':inserty:', yItem);
-  newStory = newStory.replace(':insertz:', zItem);
 
   story.textContent = newStory;
   story.style.visibility = 'visible';
